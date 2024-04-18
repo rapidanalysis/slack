aa13b0eafc5a9aa3f86858e453e3c8c7b9fd5519
final-gaLaxy <sebastian@genus.net.au>
2024-04-18 17:22:08 +1000

    use listeners for slack app

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,13 @@
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
+from handler.listeners import register_listeners
+
 app = App(process_before_response=True)
+
+# register all listeners for slack
+register_listeners(app)
+
 
 def lambda_handler(event, context):
     handler = SlackRequestHandler(app = app)
